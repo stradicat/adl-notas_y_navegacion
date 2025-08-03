91170a1de968c334b9eeefad3cc2d7b7fcc6763a
Dennis Mayr <dmayr.dev@gmail.com>
2025-08-03 00:52:39 -0400

    MainActivity: corregido el binding a la vista

--- app/src/main/java/dev/dmayr/notasynavegacion/MainActivity.kt
+++ app/src/main/java/dev/dmayr/notasynavegacion/MainActivity.kt
@@ -17,10 +17,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        enableEdgeToEdge()
+        setContentView(view)
+
+        ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
