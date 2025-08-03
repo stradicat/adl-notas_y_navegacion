5f1e3b7171f5c37df4d2331cc12cc81715d0b5d8
Dennis Mayr <dmayr.dev@gmail.com>
2025-08-03 04:49:23 -0400

    NotasViewModel: adición de variables *LiveData y métodos de búsqueda entre las notas

--- app/src/main/java/dev/dmayr/notasynavegacion/viewmodel/NotasViewModel.kt
+++ app/src/main/java/dev/dmayr/notasynavegacion/viewmodel/NotasViewModel.kt
@@ -3,6 +3,8 @@ package dev.dmayr.notasynavegacion.viewmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import dev.dmayr.notasynavegacion.data.NotaBBDD
 import dev.dmayr.notasynavegacion.model.Nota
@@ -12,12 +14,51 @@ import kotlinx.coroutines.launch
 
 class NotasViewModel(application: Application) : AndroidViewModel(application) {
     private val repository: NotaRepository
-    val notas: LiveData<List<Nota>>
+    private val _cadenaDeBusqueda = MutableLiveData<String>("")
+
+    val notas = MediatorLiveData<List<Nota>>()
+
+    private val todasLasNotas: LiveData<List<Nota>>
+    private var fuenteDeBusquedaActual: LiveData<List<Nota>>? = null
 
     init {
         val dao = NotaBBDD.getDatabase(application).notaDao()
         repository = NotaRepository(dao)
-        notas = repository.todasLasNotas
+        todasLasNotas = repository.todasLasNotas
+
+        crearFuente(todasLasNotas)
+
+        _cadenaDeBusqueda.observeForever { consulta ->
+            fuenteDeBusquedaActual?.let {
+                notas.removeSource(it)
+            }
+            if (consulta.isBlank()) {
+                notas.addSource(todasLasNotas) {
+                    notas.value = it
+                }
+                fuenteDeBusquedaActual = todasLasNotas
+            } else {
+                val fuenteDeBusqueda = repository.buscarNotas(consulta)
+                notas.addSource(fuenteDeBusqueda) {
+                    notas.value = it
+                }
+                fuenteDeBusquedaActual = fuenteDeBusqueda
+            }
+        }
+    }
+
+    private fun crearFuente(nuevaFuente: LiveData<List<Nota>>) {
+        if (fuenteDeBusquedaActual != null) {
+            notas.removeSource(fuenteDeBusquedaActual!!)
+        }
+        fuenteDeBusquedaActual = nuevaFuente
+        notas.addSource(nuevaFuente) {
+            notas.value = it
+        }
+    }
+
+    fun buscarNotas(consulta: String) {
+        _cadenaDeBusqueda.value = consulta
     }
 
     suspend fun agregarNota(nota: Nota): Long {
